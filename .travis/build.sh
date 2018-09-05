7018459ba5454943aef046ee537727be59e5a4bf
insomniac <insomniacslk@users.noreply.github.com>
2018-09-05 16:47:26 +0100

    Added musl dependency (#3)

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,6 +7,7 @@ KEXEC_TOOLS_VERSION=tags/v2.0.17
 git clone https://github.com/horms/kexec-tools.git
 cd kexec-tools
 git checkout -b "${KEXEC_TOOLS_VERSION}"
+./bootstrap
 ./configure
 make
-
+strip build/sbin/*
