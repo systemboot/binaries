1e4886acd76467ddca16eb100d363e7998587493
Andrea Barberio <insomniac@slackware.it>
2019-03-29 23:15:12 +0000

    Fix kexec-tools build and optimize for size where possible
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -exu
 
 BASEDIR="${PWD}"
 OUTDIR="${BASEDIR}/build"
@@ -16,6 +16,8 @@ git clone git://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git
 cd kexec-tools
 git checkout -b "${KEXEC_TOOLS_VERSION}"
 ./bootstrap
+# just optimize for space. Kexec uses kernel headers so we cannot use musl-gcc
+# here. See https://wiki.musl-libc.org/faq.html#Q:-Why-am-I-getting- 
 CFLAGS=-Os ./configure
 make
 strip build/sbin/kexec
@@ -27,7 +29,9 @@ cd "${OUTDIR}"
 git clone https://review.coreboot.org/cgit/flashrom.git
 cd flashrom
 git checkout -b "${FLASHROM_VERSION}"
-make
+# no musl-gcc here either, as flashrom needs libpci-dev (we may remove PCI
+# programmers from the build at a later stage though)
+CFLAGS=-Os make
 strip flashrom
 du -hs flashrom
 ldd flashrom
@@ -38,6 +42,6 @@ wget "http://pyropus.ca/software/memtester/old-versions/memtester-${MEMTESTER_VE
 tar xvzf "memtester-${MEMTESTER_VERSION}".tar.gz
 ln -s "memtester-${MEMTESTER_VERSION}" memtester
 cd "memtester-${MEMTESTER_VERSION}"
-make # build statically
+CFLAGS=-Os CC=musl-gcc make # build statically
 du -hs memtester
 ldd memtester
